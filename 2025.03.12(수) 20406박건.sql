bb30d0644b25e975cf48a11a85a518e2d19b4f83
Ledbendy <aa01042481282@gmail.com>
2025-03-12 13:00:18 +0900

    Add files via upload

---
+++ 2025.03.12(수) 20406박건.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Address(
+Address1 varchar(255),
+Address2 varchar(255),
+Address3 varchar(255),
+Posteal_code varchar(255),
+Cllient_ID varchar(255));
+
+Insert Into Address VALUES('경기', '안양시', '동안구', '관악대로', '2345');
+
+drop TABLE Address;
+
+SELECT * from Address;
